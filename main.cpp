dfd0b5d60e8144916320c4c9a64d49071124ff16
shiba <shiba@shiba.com>
2019-05-08 19:59:20 -0700

    matplotlibcppで表示できるようなmain.cppにした

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <vector>
 #include "BicycleModel.hpp"
-#include "D:/home/Sources/gnuplot/gnuplot.h"
+#include "matplotlibcpp.h"
 
 using namespace std;
-using namespace gnuplot;
+namespace plt = matplotlibcpp;
+
 
 int main() {
     BicycleModel car1(0.0,
-        30 / 3.6,
+        100 / 3.6,
         1500,
         1.0453,
         1.7546,
@@ -23,19 +25,18 @@ int main() {
         0.0001);
 
 
-    ofstream ofs("C:/Users/0133752/Desktop/aaa.csv");
 
     vector<double> x;
     vector<double> y;
     vector<double> Bf;
     vector<double> step;
 
-    double steer = 10.0 * M_PI / 180.0;
+    double steer = 5.0 * M_PI / 180.0;
     double steerd = steer * 0.002;
     car1.Step();
     car1.SetSteer(steer);
-    for (int i = 0; i < 10 / 0.0001; ++i) {
-        if (i < 1000) {
+    for (int i = 0; i < 5 / 0.0001; ++i) {
+        if (i < 10000) {
             car1.SetSteer(steer);
         }
         else {
@@ -49,21 +50,13 @@ int main() {
         //cout << "yawrate " << car1.yaw_rate * 180 / M_PI << endl;
         //cout << "x " << car1.x * 180 / M_PI << endl;
         //cout << "y " << car1.y * 180 / M_PI << endl;
-        ofs << car1.x << "," << car1.y << "," << steer << "," << car1.Bf << "," << car1.Br << "," << car1.dFf << "," << car1.Ff << endl;
         x.push_back(car1.x);
         y.push_back(car1.y);
         step.push_back(i);
         Bf.push_back(car1.Bf);
     }
+    plt::plot(x, y);
+    plt::show();
 
-    CGnuplot gp;
-    gp.SetLabel("x", "y");
-    gp.Plot(x, y);
-
-    CGnuplot gp2;
-    gp2.SetLabel("step", "Bf");
-    gp2.Plot(step, Bf);
-
-    __KEYWAIT__;
     return 0;
 }
